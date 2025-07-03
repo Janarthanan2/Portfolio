390cd4adf048f067f3452465c3440b996ea242d6
Janarthanan2 <janartanan47@gmail.com>
2025-07-03 13:58:42 +0530

    Fix vite build script

--- project/src/components/Hero.tsx
+++ project/src/components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ParticleBackground from './ParticleBackground';
 import TypewriterText from './TypewriterText';
 import janarthananImage from '../assets/Janarthanan.jpg';
+import janarthananResume from '../assets/Janarthanan_Resume.pdf';
 
 const Hero: React.FC = () => {
   return (
@@ -23,7 +24,7 @@ const Hero: React.FC = () => {
           <p>AI & Data Science Enthusiast | Software Developer</p>
           <div className="hero-buttons">
             <a href="mailto:janartanan47@gmail.com" className="btn">Hire Me</a>
-            <a href="#" className="btn" download>Download Resume</a>
+            <a href={janarthananResume} className="btn" download>Download Resume</a>
           </div>
         </div>
       </div>
